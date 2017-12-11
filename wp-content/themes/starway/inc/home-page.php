1a8bf3d848908a194c269596740259e0a03b9a2a
tarasbzovskyy <t.bzovskyy@gmail.com>
2017-12-11 16:51:00 +0200

    commit

--- wp-content/themes/starway/inc/home-page.php
+++ wp-content/themes/starway/inc/home-page.php
@@ -37,98 +37,110 @@ $arrContextOptions=array(
         echo file_get_contents(get_template_directory_uri()."/assets/img/parallels-right.svg", false, stream_context_create($arrContextOptions));
     ?>
 </section>
-<section id="portfolio" class="group">
-    <?php
-        echo file_get_contents(get_template_directory_uri()."/assets/img/services.svg", false, stream_context_create($arrContextOptions));
-    ?>
-    <div class="container">
-        <div class="row">
-            <div class="container-skew">
-                <div class="skew-center">
-                    <div class="block-skew skew-left block-item skew-space">
-                    </div>
-                    <div class="block-skew skew-left block-item skew-space">
-                    </div>
-                    <div class="block-skew skew-left block-item skew-plus-9 ">
-                        <span>
-                            <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/1.svg", false, stream_context_create($arrContextOptions)); ?>
-                        </span>
-                    </div>
-                    <div class="block-skew skew-left block-item skew-space">
-                    </div>
+<section class="group">
+    <div id="portfolio" class="group">
+        <div class="container">
+            <div class="fade-block"></div>
+            <div class="row space-top-x2">
+                <div class="section-title light-text group" title="Our Works">
+                    Our Works
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="container-skew">
-                <div class="skew-center">
-                    <div class="block-skew skew-left block-item skew-space">
-                    </div>
-                    <div class="block-skew skew-left block-item skew-plus-45">
-                          <span>
-                             <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/2.svg", false, stream_context_create($arrContextOptions)); ?>
-                          </span>
-                    </div>
-                    <div class="block-skew skew-left block-item skew-plus-45">
-                        <span>
-                             <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/3.svg", false, stream_context_create($arrContextOptions)); ?>
-                        </span>
-                    </div>
-                    <div class="block-skew skew-left block-item skew-space">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="container-skew">
-                <div class="skew-center">
-                    <div class="block-skew skew-left block-item">
-                        <span>
-                             <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/4.svg", false, stream_context_create($arrContextOptions)); ?>
-                        </span>
-                    </div>
-                    <div class="block-skew skew-left block-item">
-                        <span>
-                             <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/5.svg", false, stream_context_create($arrContextOptions)); ?>
-                        </span>
-                    </div>
-                    <div class="block-skew skew-left block-item">
-                        <span>
-                             <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/6.svg", false, stream_context_create($arrContextOptions)); ?>
-                        </span>
-                    </div>
-                    <div class="block-skew skew-left block-item">
-                        <span>
-                            <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/7.svg", false, stream_context_create($arrContextOptions)); ?>
-                        </span>
+            <div class="group">
+                <div class="skew-left accent-block accent-red"></div>
+                <div class="row">
+                    <div class="container-skew">
+                        <div class="skew-center">
+                            <div class="block-skew skew-left block-item skew-space">
+                            </div>
+                            <div class="block-skew skew-left block-item skew-space">
+                            </div>
+                            <div class="block-skew skew-left block-item skew-plus-9 ">
+                            <span>
+                                <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/1.svg", false, stream_context_create($arrContextOptions)); ?>
+                            </span>
+                            </div>
+                            <div class="block-skew skew-left block-item skew-space">
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="container-skew">
-                <div class="skew-center">
-                    <div class="block-skew skew-left block-item skew-space">
+                <div class="row">
+                    <div class="container-skew">
+                        <div class="skew-center">
+                            <div class="block-skew skew-left block-item skew-space">
+                            </div>
+                            <div class="block-skew skew-left block-item skew-plus-45">
+                              <span>
+                                 <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/2.svg", false, stream_context_create($arrContextOptions)); ?>
+                              </span>
+                            </div>
+                            <div class="block-skew skew-left block-item skew-plus-45">
+                            <span>
+                                 <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/3.svg", false, stream_context_create($arrContextOptions)); ?>
+                            </span>
+                            </div>
+                            <div class="block-skew skew-left block-item skew-space">
+                            </div>
+                        </div>
                     </div>
-                    <div class="block-skew skew-left block-item skew-space">
+                </div>
+                <div class="row">
+                    <div class="container-skew">
+                        <div class="skew-center">
+                            <div class="block-skew skew-left block-item">
+                            <span>
+                                 <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/4.svg", false, stream_context_create($arrContextOptions)); ?>
+                            </span>
+                            </div>
+                            <div class="block-skew skew-left block-item">
+                            <span>
+                                 <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/5.svg", false, stream_context_create($arrContextOptions)); ?>
+                            </span>
+                            </div>
+                            <div class="block-skew skew-left block-item">
+                            <span>
+                                 <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/6.svg", false, stream_context_create($arrContextOptions)); ?>
+                            </span>
+                            </div>
+                            <div class="block-skew skew-left block-item">
+                            <span>
+                                <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/7.svg", false, stream_context_create($arrContextOptions)); ?>
+                            </span>
+                            </div>
+                        </div>
                     </div>
-                    <div class="block-skew skew-left block-item skew-minus-45">
-                        <span>
-                             <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/8.svg", false, stream_context_create($arrContextOptions)); ?>
-                        </span>
-                    </div>
-                    <div class="block-skew skew-left block-item show-more skew-minus-45">
-                        <a href="#" class="skew-more">
-                            <span class="unskew-child">More</span>
-                        </a>
+                </div>
+                <div class="row">
+                    <div class="container-skew">
+                        <div class="skew-center">
+                            <div class="block-skew skew-left block-item skew-space">
+                            </div>
+                            <div class="block-skew skew-left block-item skew-space">
+                            </div>
+                            <div class="block-skew skew-left block-item skew-minus-45">
+                            <span>
+                                 <?php  echo file_get_contents(get_template_directory_uri()."/assets/img/portfolio/8.svg", false, stream_context_create($arrContextOptions)); ?>
+                            </span>
+                            </div>
+                            <div class="block-skew skew-left block-item show-more skew-minus-45">
+                                <a href="#" class="skew-more">
+                                    <span class="unskew-child">More</span>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-
-
-
+    <div class="portfolio-footer">
+        <?php
+            echo file_get_contents(get_template_directory_uri()."/assets/img/services-foot.svg", false, stream_context_create($arrContextOptions));
+        ?>
+    </div>
+    <?php
+        echo file_get_contents(get_template_directory_uri()."/assets/img/triangles.svg", false, stream_context_create($arrContextOptions));
+    ?>
 </section>
 <?php get_footer(); ?>
